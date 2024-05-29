ce24d8594aa58d5d7c0062abfe788df7a95a1eef
mostafazhrn <mostafazhrn@gmail.com>
2024-05-29 21:07:02 +0300

    fix-6-finalUser

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,30 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', filename = '') {
-  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
+export default async function handleProfileSignup(firstName, lastName, filename) {
+  const usr = {
+    status: 'pending',
+    value: '',
+  };
+  const photo = {
+    status: 'pending',
+    value: '',
+  };
+  try {
+    const register = await signUpUser(firstName, lastName);
+    usr.status = 'fulfilled';
+    usr.value = register;
+  } catch (error) {
+    usr.status = 'rejected';
+    usr.value = error.toString();
+  }
+  try {
+    const photoUpload = await uploadPhoto(filename);
+    photo.status = 'fulfilled';
+    photo.value = photoUpload;
+  } catch (error) {
+    photo.status = 'rejected';
+    photo.value = error.toString();
+  }
+  return { usr, photo };
 }
-
-export default handleProfileSignup;
