3f98b26108a523759a95767ad8267fae60db1819
mostafazhrn <mostafazhrn@gmail.com>
2024-05-29 18:59:43 +0300

    3-all-Push

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((respond) => {
+      const { body } = respond[0];
+      const { firstName, lastName } = respond[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
