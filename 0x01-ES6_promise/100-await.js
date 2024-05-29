f9822d4f16f081e742cce12bad37a9a7d3692fc8
mostafazhrn <mostafazhrn@gmail.com>
2024-05-29 22:14:08 +0300

    update_100_await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    return {
-      photo,
-      user,
-    };
-  } catch (error) {
-    return {
-      photo: null,
-      user: null,
-    };
-  }
+  const photo = await uploadPhoto();
+  const user = await createUser();
+  return {
+    photo,
+    user,
+  };
 }
