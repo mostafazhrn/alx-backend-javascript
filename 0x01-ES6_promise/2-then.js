26d59f51ab4b431b4b1616037e9e78fa5025410a
mostafazhrn <mostafazhrn@gmail.com>
2024-05-29 18:27:38 +0300

    fix2Then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
-  }, () => {
-    console.log('Got an error from the API');
-    return {};
-  });
+  return promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
