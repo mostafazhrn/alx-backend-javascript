8d7ee278646a99f55ad27f18cef48926ccfefa6f
mostafazhrn <mostafazhrn@gmail.com>
2024-05-29 20:29:14 +0300

    final_user_push

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,8 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignup(firstName = '', lastName = '', filename = '') {
+  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]);
+}
+
+export default handleProfileSignup;
