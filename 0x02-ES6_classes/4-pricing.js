dd607009af1f80c85e310a9b87fb49440e159182
mostafazhrn <mostafazhrn@gmail.com>
2024-05-30 20:07:59 +0300

    push-pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(nuevoAmount) {
+    if (typeof nuevoAmount === 'number') {
+      this._amount = nuevoAmount;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(nuevoCurrency) {
+    if (nuevoCurrency instanceof Currency) {
+      this._currency = nuevoCurrency;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
