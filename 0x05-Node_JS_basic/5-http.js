61c9571ee2b6de60a06ef36ce98f3e0011dcc9a5
mostafazhrn <mostafazhrn@gmail.com>
2024-07-22 18:19:31 -0400

    push_task5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  // continue the code here
+  if (req.url === '/') res.write('Hello Holberton School!');
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const donne = await countStudents(process.argv[2]);
+      res.end(`${donne.join('\n')}`);
+    } catch (err) {
+      res.end(err.message);
+    }
+  }
+  res.end();
+});
+app.listen(1245);
+module.exports = app;
