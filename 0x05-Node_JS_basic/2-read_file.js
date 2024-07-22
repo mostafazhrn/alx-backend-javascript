d1bf1e7d3403aa1acdfa3ac6cc589aa3ab63dfcd
mostafazhrn <mostafazhrn@gmail.com>
2024-07-22 17:12:11 -0400

    push_task2_fix

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-  try{
-    let data = fs.readFileSync(path, 'utf8').toString().split('\n');
-    donne = data.slice(1, data.length - 1);
+  try {
+    let donne = fs.readFileSync(path, 'utf8').toString().split('\n');
+    donne = donne.slice(1, donne.length - 1);
     console.log(`Number of students: ${donne.length}`);
     const sujs = {};
     for (const row of donne) {
@@ -12,7 +12,7 @@ const countStudents = (path) => {
       sujs[student[3]].push(student[0]);
     }
     for (const suj in sujs) {
-      if (suj) console.log(`Number of students in ${suj}: ${sujs[suj].length}. List: ${sujs[suj].join(', ')}`); 
+      if (suj) console.log(`Number of students in ${suj}: ${sujs[suj].length}. List: ${sujs[suj].join(', ')}`);
     }
   } catch (err) {
     throw new Error('Cannot load the database');
