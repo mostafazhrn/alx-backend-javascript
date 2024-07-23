cd8826be7e0d6a0b6ad5cc942ada467996a2da16
mostafazhrn <mostafazhrn@gmail.com>
2024-07-22 20:04:49 -0400

    push_task3_fix5

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -8,33 +8,32 @@ function countStudents(path) {
         if (err) {
           reject(new Error('Cannot load the database'));
           return;
-      }
-      const respo = [];
-      let mesage;
-      const conte = data.split('\n');
-
-      let students = conte.filter((item) => item.length);
-      students = students.map((item) => item.split(','));
-      const szStudents = students.length ? students.length - 1 : 0;
-      mesage = `Number of students: ${szStudents}`;
-      console.log(mesage);
-
-      respo.push(mesage);
-      const fields = {};
-      for (const x in students) {
-        if (x !== 0) {
-          if (!fields[students[x][3]]) fields[students[x][3]] = [];
-          fields[students[x][3]].push(students[x][0]);
         }
-      }
-      delete fields.field;
-      for (const cle of Object.keys(fields)) {
-        mesage = `Number of students in ${cle}: ${fields[cle].length}. List: ${fields[cle].join(', ')}`;
+        const respo = [];
+        let mesage;
+        const conte = data.split('\n');
+
+        let students = conte.filter((item) => item.length);
+        students = students.map((item) => item.split(','));
+        const szStudents = students.length ? students.length - 1 : 0;
+        mesage = `Number of students: ${szStudents}`;
         console.log(mesage);
+
         respo.push(mesage);
-      }
-      resolve(respo);
-    });
+        const fields = {};
+        for (const x in students) {
+          if (x !== 0) {
+            if (!fields[students[x][3]]) fields[students[x][3]] = [];
+            fields[students[x][3]].push(students[x][0]);
+          }
+        }
+        delete fields.field;
+        for (const cle of Object.keys(fields)) {
+          mesage = `Number of students in ${cle}: ${fields[cle].length}. List: ${fields[cle].join(', ')}`;
+          console.log(mesage);
+          respo.push(mesage);
+        }
+        resolve(respo);
+      });
   });
 }
-module.exports = countStudents;
