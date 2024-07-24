4cf8581c970e631e62ac6021b83eb0e2bc142aca
mostafazhrn <mostafazhrn@gmail.com>
2024-07-24 18:31:44 -0400

    push_task0_fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,16 +4,16 @@ const mocha = require('mocha');
 
 
 describe('calculateNumber', () => {
-    it ('return the sum', () => {
-        assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1.1, 3.1), 4);
-        assert.strictEqual(calculateNumber(1.4, 3.4), 5);
-        assert.strictEqual(calculateNumber(1.5, 3.5), 5);
-        assert.strictEqual(calculateNumber(1.6, 3.6), 6);
-        assert.strictEqual(calculateNumber(1.9, 3.9), 6);
+    it('return the sum', () => {
+      assert.strictEqual(calculateNumber(1, 3), 4);
+      assert.strictEqual(calculateNumber(1.1, 3.1), 4);
+      assert.strictEqual(calculateNumber(1.4, 3.4), 5);
+      assert.strictEqual(calculateNumber(1.5, 3.5), 5);
+      assert.strictEqual(calculateNumber(1.6, 3.6), 6);
+      assert.strictEqual(calculateNumber(1.9, 3.9), 6);
     });
-    it ('check arguments' () => {
-        assert.strictEqual(isNaN(calculateNumber(1)), true);
-        assert.strictEqual(isNaN(calculateNumber()), true);
+    it('check arguments', () => {
+      assert.strictEqual(isNaN(calculateNumber(1)), true);
+      assert.strictEqual(isNaN(calculateNumber()), true);
     });
 });
