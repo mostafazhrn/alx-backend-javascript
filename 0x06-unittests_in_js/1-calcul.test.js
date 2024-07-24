05f31181bd8834dc6f4a1b62296a113917595049
mostafazhrn <mostafazhrn@gmail.com>
2024-07-24 19:20:24 -0400

    push_task1_fix

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -5,7 +5,7 @@ const assert = require('assert');
 describe('calculateNumber type == SUM', () => {
     it ('return the sum', () => {
         assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
-        assert.strictEqual(calculateNumber('SUM', 1.1, 3.1), 4);
+        assert.strictEqual(calculateNumber('SUM', 1.1, 3.1), 5);
         assert.strictEqual(calculateNumber('SUM', 1.4, 3.4), 5);
         assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
     });
@@ -13,16 +13,16 @@ describe('calculateNumber type == SUM', () => {
 describe('calculateNumber type == SUBTRACT', () => {
     it ('return the difference', () => {
         assert.strictEqual(calculateNumber('SUBTRACT', 5, 3), 2);
-        assert.strictEqual(calculateNumber('SUBTRACT', 4.1, 3.5), 0);
-        assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 3.4), -2);
+        assert.strictEqual(calculateNumber('SUBTRACT', 6, 3), 3);
+        assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5, -4);
         assert.strictEqual(calculateNumber('SUBTRACT', 0, 0), 0);
     });
 });
 describe('calculateNumber type == DIVIDE', () => {
     it ('return the division', () => {
         assert.strictEqual(calculateNumber('DIVIDE', 5, 3), 2);
-        assert.strictEqual(calculateNumber('DIVIDE', 4.1, 3.5), 1);
-        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 3.4), 0);
+        assert.strictEqual(calculateNumber('DIVIDE', 6, 2), 3);
+        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
         assert.strictEqual(calculateNumber('DIVIDE', 0, 0), 'Error');
     });
 });
